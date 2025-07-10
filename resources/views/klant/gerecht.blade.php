3b0447739e4664bfceb7dd8f84700c3dc20e265b
MossGuy <milanpallas@hotmail.nl>
2025-07-10 13:57:50 +0200

    beschrijving toegevoegd aan gerecht weergave en nieuwe terug link url toegepast

--- resources/views/klant/gerecht.blade.php
+++ resources/views/klant/gerecht.blade.php
@@ -2,10 +2,18 @@
 
 @section('main')
     <section class="p-6">
+        <a href="{{ url()->previous() }}" class="text-sm text-teal-700 hover:underline mb-4 flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            </svg>
+            <span class="ml-1">Terug</span>
+        </a>
+
         <figure class="bg-white rounded-xl shadow-md overflow-hidden max-w-md mx-auto">
             <img src="{{ asset('images/dish_placeholder.png') }}" alt="Foto van {{ $gerecht->naam }}" class="w-full h-64 object-cover">
             <figcaption class="p-4">
                 <h1 class="text-2xl font-bold">{{ $gerecht->naam }}</h1>
+                <p>{{ $gerecht->beschrijving }}.</p>
                 <p class="text-stone-700 my-2">€{{ number_format($gerecht->prijs, 2, ',', '.') }}</p>
                 <form action="{{ route('bestelling.store') }}" method="POST">
                     @csrf
