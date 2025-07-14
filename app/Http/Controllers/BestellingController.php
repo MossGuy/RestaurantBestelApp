f9f65e249f0593a2300a6f96ae70d704f44e16eb
MossGuy <milanpallas@hotmail.nl>
2025-07-14 12:04:39 +0200

    bestellingen view in admin map afgemaakt

--- app/Http/Controllers/BestellingController.php
+++ app/Http/Controllers/BestellingController.php
@@ -77,7 +77,7 @@ class BestellingController extends Controller
 
         return view("$mapNaam.bestellingen", [
             'bestellingen' => $bestellingen,
-            'sessie_id' => $session_id, // eventueel meegeven aan de view
+            'sessie_id' => $session_id,
         ]);
     }
 
