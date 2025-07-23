e0803aeaf7b7441cbe4fedec76957018e8a26f00
MossGuy <milanpallas@hotmail.nl>
2025-07-23 16:32:07 +0200

    terug button url aangepast

--- resources/views/admin/open_bestellingen.blade.php
+++ resources/views/admin/open_bestellingen.blade.php
@@ -4,7 +4,7 @@
     <section>
         <div class="flex justify-between items-center">
             <h1 class="text-2xl font-bold mb-6">Open bestellingen</h1>
-            <p><a href="{{ url()->previous() }}" class="text-teal-700 p-2 border border-2 rounded-md hover:text-teal-700 font-semibold">Terug</a></p>
+            <p><a href="{{ url('admin') }}" class="text-teal-700 p-2 border border-2 rounded-md hover:text-teal-700 font-semibold">Terug</a></p>
         </div>
     </section>
 
